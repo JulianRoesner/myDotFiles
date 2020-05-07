55f72ccdb874aa1c7c7664e93680982f7c6c17fc
JulianRoesner <julian_roesner@gmx.de>
2020-05-07 13:47:08 +0200

    added brew.sh

---
+++ brew.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+#install languages
+echo "Installing programming languages..."
+brew install python3
+brew install R
+brew install javascript
+brew install 
+
+#install programs
+echo "Installing programming applications..."
+brew install pip3
+brew install pipenv
+brew install node
+brew install sublime-text
--- setup.sh
+++ setup.sh
@@ -9,18 +9,7 @@
 
 
 #install languages
-echo "Installing programming languages..."
-brew install python3
-brew install R
-brew install javascript
-brew install 
-
-#install programs
-echo "Installing programming applications..."
-brew install pip3
-brew install pipenv
-brew install node
-brew install sublime-text
+sh brew.sh
 
 
 ##############################################################################
