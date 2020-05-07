3a15bcba4db0c1384a6810d8f20e2936d30592be
JulianRoesner <julian_roesner@gmx.de>
2020-05-07 13:45:35 +0200

    Added echos to setup.sh. Small changes to Readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # My Dotfiles
 
 This is the collection of my dotfiles and setup scripts.
+
+# Setup
+The setup.sh will install all necessary applications and scripts. It will also apply all the settings.
+
+## Brew & Brew Cask
+Brew will install all the applications listed in brew.sh
+
+## Pip
+Pip will install all the everything in requirements.txt
--- setup.sh
+++ setup.sh
@@ -9,12 +9,14 @@
 
 
 #install languages
+echo "Installing programming languages..."
 brew install python3
 brew install R
 brew install javascript
 brew install 
 
 #install programs
+echo "Installing programming applications..."
 brew install pip3
 brew install pipenv
 brew install node
@@ -24,6 +26,5 @@ brew install sublime-text
 ##############################################################################
 ##                                   Pip                                    ##
 ##############################################################################
+echo "Installing python libraries using pip..."
 pip3 install -r requirements.txt
-
-
