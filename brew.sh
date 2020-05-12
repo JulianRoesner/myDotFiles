7f7611276cebe84e14957634adbffceb99b75b7c
JulianRoesner <julian_roesner@gmx.de>
2020-05-12 12:41:03 +0200

    added .vimrc , .bash_profile with syntax highlighting and color coding

--- brew.sh
+++ brew.sh
@@ -5,11 +5,17 @@ echo "Installing programming languages..."
 brew install python3
 brew install R
 brew install javascript
-brew install 
+brew install haskell
+brew cask install Java
 
 #install programs
 echo "Installing programming applications..."
 brew install pip3
-brew install pipenv
 brew install node
 brew install sublime-text
+brew install heroku
+
+#install git and tools
+echo "Installing git and related tools..."
+brew install git
+brew install git-extras
