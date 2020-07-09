ee30fb7236a041b64ab0f23c77a11c7c1c731c0d
JulianRoesner <julian_roesner@gmx.de>
2020-07-09 11:54:23 +0200

    Localisation for macOS

--- mac/mac.sh
+++ mac/mac.sh
@@ -5,3 +5,14 @@ xcode-select --install
 
 # Update all software 
 softwareupdate --install -a
+
+# Set language and text formats
+defaults write NSGlobalDomain AppleLanguages -array "de" "en-US"
+defaults write NSGlobalDomain AppleLocale "de_DE"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
+defaults write NSGlobalDomain AppleTemperatureUnit -string "Celsius"
+
+# Set the timezone; see `sudo systemsetup -listtimezones` for other values
+sudo systemsetup -settimezone "Europe/Berlin" > /dev/null
+
