8ce9e4801952230cb219798c4211f4931bad2aaa
Julian <julian_roesner@gmx.de>
2026-04-20 23:40:14 +0200

    Add additional brew installations for tools and apps

--- brew.sh
+++ brew.sh
@@ -34,14 +34,20 @@ brew install sublime-text
 brew install heroku
 brew install node
 brew install swiftlint
+brew install maven
+brew install pinentry
+brew install pinentry-mac
 
 #install cli infrastructure tools
 brew install terraform
+brew install tfenv
 brew install kubernetes-cli
 brew install kubectx
 brew install kubeseal
 brew install lefthook 
 brew install talisman
+brew install openvpn
+brew install k9s
 brew install openvpn
 
 #install programms with cask 
@@ -49,9 +55,14 @@ brew install --cask slack
 brew install --cask pycharm-ce
 brew install --cask google-chrome
 brew install --cask firefox
+brew install --cask 1password-cli
 
 #install docker
 brew install colima
+brew install docker
+brew install docker-buildx
+brew install docker-completion
+brew install docker-compose
 ./docker.sh
 
 # Remove outdated versions from the cellar.
